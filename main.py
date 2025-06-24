182fdd178fc55544f332070cc1e07eb3b1f91f50
keirakeiraaaa <winner908809@gmail.com>
2025-06-24 09:53:12 -0600

    was able to get this to work but I need a new branch to work on ability to populate dict without inputting it

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from stats import get_word_count
+from stats import get_num_words
+from stats import get_num_character
 
 def main():
       book_path = "books/frankenstein.txt"
       book_text = get_book_text(book_path)
 
-      number_words = get_word_count(book_text)
+      number_words = get_num_words(book_text)
       print(f"{number_words} words found in the document")
+
+      number_characters = get_num_character(book_text)
+      print(number_characters)
 
 def get_book_text(book_path):
         with open(book_path) as f:
--- stats.py
+++ stats.py
@@ -1,9 +1,16 @@
-def get_word_count(book_text):
-      # convert book_text into a string
-      text_string = str(book_text)
-
+def get_num_words(book_text):
       # then convert that string into a list
-      text_list = text_string.split()
+      text_list = book_text.split()
 
       # then have computer count number of items inside list :)
       return len(text_list)
+
+def get_num_character(book_text):
+    count_num = {
+         "a" : 0, "b" : 0, "c" : 0, "d" : 0, "e" : 0, "f" : 0, "g" : 0, "h" : 0, "i" : 0, "j" : 0, "k" : 0, "l" : 0, "m" : 0, "n" : 0, "o" : 0, "p" : 0, "q" : 0, "r" : 0, "s" : 0, "t" : 0, "u" : 0, "v" : 0, "w" : 0, "x" : 0, "y" : 0, "z" : 0
+    }
+
+    for letters in book_text:
+         if letters in count_num:
+              count_num[letters] += 1
+    return count_num
