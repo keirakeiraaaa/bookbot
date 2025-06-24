ee2a119c59db74d78caddbad4dd58a3f3f14cbf8
keirakeiraaaa <winner908809@gmail.com>
2025-06-24 10:46:31 -0600

    finished up logging to make it look pretty

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from stats import get_num_words
 from stats import get_num_character
+from stats import get_sort_data
 
 def main():
       book_path = "books/frankenstein.txt"
       book_text = get_book_text(book_path)
 
       number_words = get_num_words(book_text)
-      print(f"{number_words} words found in the document")
 
       number_characters = get_num_character(book_text)
-      print(number_characters)
+      # print(number_characters)
+
+      sort_data = get_sort_data(number_characters)
+
+      print("============ BOOKBOT ============")
+      print(f"Analyzing book found at {book_path}...")
+      print("----------- Word Count ----------")
+      print(f"Found {number_words} total words")
+      print("--------- Character Count -------")
+
+      for item in sort_data:
+            print(item["char"], item["num"])
+      
+      print("============= END ===============")
+      
+      
 
 def get_book_text(book_path):
         with open(book_path) as f:
--- stats.py
+++ stats.py
@@ -6,13 +6,23 @@ def get_num_words(book_text):
       return len(text_list)
 
 def get_num_character(book_text):
+    book_text = book_text.lower()
     count_num = {}
-
-
     for letter in book_text:
         if letter in count_num:
             count_num[letter] += 1
         else:
             count_num[letter] = 1
-
     return count_num
+
+def sort_on(count):
+    return count["num"]
+
+def get_sort_data(number_char):
+    sorted_list = []
+    for letter in number_char:
+        if letter.isalpha() == True:
+            combined_dict = {"char" : letter, "num" : number_char[letter]}
+            sorted_list.append(combined_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
